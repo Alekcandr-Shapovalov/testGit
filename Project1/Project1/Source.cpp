af118ac052a9eb7eee49e75b1e8216594b71b904
Aleksandr-Shapovalov <gpocsta@gmail.com>
2020-08-19 09:37:34 +0300

    #1

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	cout << "Hello" << endl;
+	cout << "Hello" << endl;
+	cout << "Hello" << endl;
+
+	system("pause");
+}
